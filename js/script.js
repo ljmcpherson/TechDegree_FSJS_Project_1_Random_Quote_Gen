e0b6023e36363475d0a504366def3d7e1dd0c45f
ljmcpherson <60832047+ljmcpherson@users.noreply.github.com>
2022-08-15 17:18:41 -0700

    copy and pasted printQuote() instructions

--- js/script.js
+++ js/script.js
@@ -72,6 +72,31 @@ console.log(quotes);
 /***
  * `printQuote` function
 ***/
+function printQuote() {
+  // 1. Create a variable that calls the getRandomQuote() 
+  // function
+
+  // 2. Create a variable that initiates your HTML string with 
+  // the first two <p></p> elements, their classNames, 
+  // and the quote and source properties, but leave off 
+  // the second closing `</p>` tag for now
+
+  // 3. Use an if statement to check if the citation property 
+  // exists, and if it does, concatenate a <span></span> 
+  // element, appropriate className, and citation property 
+  // to the HTML string
+
+  // 4. Use an if statement to check of the year property exists, 
+  // and if it does, concatenate a <span></span> element, 
+  // appropriate className, and year property to the HTML 
+  //string
+
+  // 5. After the two if statements, concatenate the closing </p> 
+  // tag to the HTML string
+
+  // 6. set the innerHTML of the quote-box div to equal the 
+  // complete HTML string
+}
 
 
 
